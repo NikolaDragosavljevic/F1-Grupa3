2c974276d383c50392d8e4b2cdc9ce5dc364eb34
NikolaDragosavljevic <niki2eaper@gmail.com>
2024-05-29 12:38:38 +0200

    1/3 driver details

--- src/Components/F1AllRaceDetails.js
+++ src/Components/F1AllRaceDetails.js
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import axios from "axios";
 import Flag from "react-flagkit";
+import detailslink from '../img/link-black.png';
 
 const F1AllRaceDetails = (props) => {
-    
+
     const [isLoading, setIsLoading] = useState(true);
     const [raceDetails, setRaceDetails] = useState([]);
-    const [raceQualifiers,setRaceQualifiers] = useState([]);
+    const [raceQualifiers, setRaceQualifiers] = useState([]);
     const params = useParams();
 
     useEffect(() => {
@@ -20,30 +21,62 @@ const F1AllRaceDetails = (props) => {
         try {
             const response = await axios.get(url);
             const response1 = await axios.get(url1);
-            setRaceDetails(response.data.MRData.RaceTable.Races.Results);
+            setRaceDetails(response.data.MRData.RaceTable.Races);
             setRaceQualifiers(response1.data.MRData.RaceTable.Races.QualifyingResults);
             setIsLoading(false);
         } catch (error) {
             console.log("Axios error ", error);
         };
 
-    };    
+    };
 
 
     if (isLoading) {
         return (
-            <h1>... is (still) loading ...</h1>)
+            <h1>... is (still) loading ...</h1>);
     }
+
 
 
 
     return <div>
         <div>
-            <Flag/>
+            <div>
+                <Flag />
+            </div>
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Country: </td>
+                            <td>{raceDetails[0].Circuit.Location.country}</td>
+                        </tr>
+                        <tr>
+                            <td>Location: </td>
+                            <td>{raceDetails[0].Circuit.Location.locality}</td>
+                        </tr>
+                        <tr>
+                            <td>Date: </td>
+                            <td>{raceDetails[0].date}</td>
+                        </tr>
+                        <tr>
+                            <td>Full Report: </td>
+                            <td><a target='_blank' rel='noopener noreferrer' href={raceDetails[0].url}><img src={detailslink} style={{ width: 15, height: 15 }} /></a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-    </div>
-}
+
+        <div>
+            <table>
+                
+            <tbody>
+
+            </tbody>
+            </table>
+        </div>
+    </div>;
+};
 
 export default F1AllRaceDetails;
-
-// race.Circuit.Location.country, race.raceName,race.Circuit.Location.locality, race.date, race.round
