69fe6ab0254d7ad21644409b576ff9cb8fe72fc8
jelena-droid <flowering.peaches@gmail.com>
2024-05-30 14:54:03 +0200

    updated driver details

--- src/Components/F1AllTeamsDetails.js
+++ src/Components/F1AllTeamsDetails.js
@@ -1,158 +1,123 @@
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import detailslink from '../img/link-black.png';
+import Flag from 'react-flagkit';
+import { getFlagCode } from "../helpers";
+import spinner from '../img/F1_chequered_flag_Animated.gif';
 
-// const F1AllTeamsDetails = ({ constructorId, year }) => {
-//     const [teamDetails, setTeamDetails] = useState(null);
-//     const [drivers, setDrivers] = useState([]);
-//     const [races, setRaces] = useState([]);
+const F1AllTeamsDetails = (props) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [teamDetails, setTeamDetails] = useState({});
+    const [driverRaces, setDriverRaces] = useState([]);
+    const params = useParams();
+    const flags = props.flags;
+    const year = props.year;
 
-//     useEffect(() => {
-//         const getTeamDetails = async () => {
-//             try {
-//                 const response = await axios.get(`http://ergast.com/api/f1/${year}/constructors/${constructorId}/constructorStandings.json`);
-//                 const constructorStandings = response.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[0];
-//                 setTeamDetails(constructorStandings);
-//                 const driverResponse = await axios.get(`http://ergast.com/api/f1/${year}/constructors/${constructorId}/drivers.json`);
-//                 const teamDrivers = driverResponse.data.MRData.DriverTable.Drivers;
-//                 setDrivers(teamDrivers);
+    useEffect(() => {
+        getTeamDetails();
+    }, []); 
 
-//                 const racesResponse = await axios.get(`http://ergast.com/api/f1/${year}.json`);
-//                 const racesData = racesResponse.data.MRData.RaceTable.Races;
-//                 setRaces(racesData);
-//             } catch (error) {
-//                 console.log("Something went wrong:", error);
-//             }
-//         };
-//         getTeamDetails();
-//     }, [constructorId, year]);
+    const getTeamDetails = async () => {
+        const constructorStandingsUrl = `http://ergast.com/api/f1/${year}/constructors/${params.id}/constructorStandings.json`;
+        const resultsUrl = `http://ergast.com/api/f1/${year}/constructors/${params.id}/results.json`;
+        try {
+            const constructorStandingsResponse = await axios.get(constructorStandingsUrl);
+            const resultsResponse = await axios.get(resultsUrl);
+            setTeamDetails(constructorStandingsResponse.data.MRData.StandingsTable.StandingsLists[0].ConstructorStandings[0]);
+            setDriverRaces(resultsResponse.data.MRData.RaceTable.Races);
+            setIsLoading(false);
+        } catch (error) {
+            console.log("Axios error:", error); 
+            setIsLoading(false); 
+        }
+    };
 
-//     if (!teamDetails) return null;
+    const calculateTotalPoints = () => {
+        let totalPoints = 0;
+        driverRaces.forEach(race => {
+            race.Results.forEach(result => {
+                totalPoints += parseInt(result.points);
+            });
+        });
+        return totalPoints;
+    };
 
-   
-//     const rounds = races.map(race => ({ round: race.round, raceName: race.raceName }));
+    const addPoints = (race) => {
+        return (race.Results[0].points + race.Results[1].points);
+    } 
 
-//     return (
-//         <div>
-//             <h3>{teamDetails.Constructor.name} Details</h3>
-//             <p>Country: {teamDetails.Constructor.nationality}</p>
-//             <p>Position: {teamDetails.position}</p>
-//             <p>Points: {teamDetails.points}</p>
-//             <p>History: <a href={teamDetails.Constructor.url} target="_blank" rel="noopener noreferrer">Open History</a></p>
-//             <p>Drivers:</p>
-//             <ul>
-//                 {drivers.map((driver, index) => (
-//                     <li key={index}>{driver.givenName} {driver.familyName} - {driver.points}</li>
-//                 ))}
-//             </ul>
-//             <p>Grand Prix:</p>
-//             <ul>
-//                 {rounds.map((round, index) => (
-//                     <li key={index}>Round {round.round} - {round.raceName}</li>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-// };
+    if (isLoading) {
+        return (
+            <div>
+                <img src={spinner} style={{ width: 250, height: 250 }} alt="Loading spinner" />
+                <h1>... data is (still) loading ...</h1>
+            </div>
+        );
+    }
 
-// export default F1AllTeamsDetails;
-
-// import { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom"
-// import axios from "axios";
-// import detailslink from '../img/link-black.png';
-// import Flag from 'react-flagkit';
-// import { getFlagCode } from "../helpers";
-
-// const F1TeamsDetails = (props) => {
-//     const [isLoading, setIsLoading] = useState(true);
-//     const [dteamsDetails, setTeamsDetails] = useState({});
-//     const [teamRaces, setTeamRaces] = useState([]);
-//     const params = useParams();
-//     const flags = props.flags;
-//     const year = props.year;
-
-//     useEffect(() => {
-//         getTeamsDetails();
-//     }, []);
-
-//     const getTeamsDetails = async () => {
-//         const driverStandingsUrl = `http://ergast.com/api/f1/${year}/constructors/${constructorId}/constructorStandings.json`;
-//         const resultsUrl = `http://ergast.com/api/f1/2013/drivers/${params.id}/results.json`;
-//         try {
-//             const driverStandingsResponse = await axios.get(driverStandingsUrl);
-//             const resultsResponse = await axios.get(resultsUrl);
-//             setDriverDetails(driverStandingsResponse.data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]);
-//             setDriverRaces(resultsResponse.data.MRData.RaceTable.Races);
-//             setIsLoading(false);
-//         } catch (error) {
-//             console.log("Axios error");
-//         };
-//     };
-
-//     if (isLoading) {
-//         return (
-//             <h1>... is (still) loading ...</h1>)
-//     }
+    
+    const driverLastNames = Array.from(new Set(driverRaces.flatMap(race => race.Results.map(result => result.Driver.familyName))));
 
 
-//     return <div>
-//         <div>
-//             <img src={require(`../img/${params.id}.jpg`)} />
-//             <Flag country={getFlagCode(flags, driverDetails.Driver.nationality)} />
-//             <p>{driverDetails.Driver.givenName}</p>
-//             <p>{driverDetails.Driver.familyName}</p>
-//         </div>
-//         <div>
-//             <table>
-//                 <tbody>
-//                     <tr>
-//                         <td>Country: </td>
-//                         <td>{driverDetails.Driver.nationality}</td>
-//                     </tr>
-//                     <tr>
-//                         <td>Team: </td>
-//                         <td>{driverDetails.Constructors[0].name}</td>
-//                     </tr>
-//                     <tr>
-//                         <td>Birth: </td>
-//                         <td>{driverDetails.Driver.dateOfBirth}</td>
-//                     </tr>
-//                     <tr>
-//                         <td>Biography: </td>
-//                         <td><a target='_blank' rel='noopener noreferrer' href={driverDetails.Driver.url}><img src={detailslink} style={{ width: 15, height: 15 }} /></a></td>
-//                     </tr>
-//                 </tbody>
-//             </table>
-//         </div>
-//         <div>
-//             <table>
+    return (
+        <div>
+            <div>
+                <Flag country={getFlagCode(flags, teamDetails.Constructor.nationality)} />
+                <p>{teamDetails.Constructor.name}</p>
+            </div>
+            <div>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Country: </td>
+                            <td>{teamDetails.Constructor.nationality}</td>
+                        </tr>
+                        <tr>
+                            <td>Position: {teamDetails.position}</td>
+                        </tr>
+                        <tr>
+                            <td>Points: </td>
+                            <td>{calculateTotalPoints()}</td>
+                        </tr>
+                        <tr>
+                            <td>History: </td>
+                            <td><a target='_blank' rel='noopener noreferrer' href={teamDetails.Constructor.url}><img src={detailslink} style={{ width: 15, height: 15 }} alt="Details link" /></a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Round</th>
+                            <th>Grand Prix</th>
+                            {driverLastNames.map((lastName, index) => (
+                                <th key={index}>{lastName}</th> 
+                            ))}
+                            <th>Points</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {driverRaces.map(race => (
+                            <tr key={race.round}>
+                                <td>{race.round}</td>
+                                <td>
+                                    <Flag country={getFlagCode(flags, race.Circuit.Location.country)} /> {race.raceName}
+                                </td>
+                                {driverLastNames.map((lastName, index) => {
+                                    const driverResult = race.Results.find(result => result.Driver.familyName === lastName && result.Constructor.constructorId === params.id);
+                                    return <td key={index}>{driverResult ? driverResult.position : "-"}</td>;
+                                })}
+                                <td>{addPoints(race)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
 
-//                 <thead>
-//                     <tr>
-//                         <th colSpan="5">Formula 1 {year} Results</th>
-//                     </tr>
-//                     <tr>
-//                         <th>Round</th>
-//                         <th>Grand Prix</th>
-//                         <th>Team</th>
-//                         <th>Grid</th>
-//                         <th>Race</th>
-//                     </tr>
-//                 </thead>
-//                 <tbody>
-//                     {driverRaces.map((race) => (
-//                         <tr key={race.raceName}>
-//                             <td>{race.round}</td>
-//                             <td>  <Flag country={getFlagCode(flags, race.Circuit.Location.country)} /> {race.raceName}</td>
-//                             <td>{race.Results[0].Constructor.name}</td>
-//                             <td>{race.Results[0].grid}</td>
-//                             <td>{race.Results[0].position}</td>
-//                         </tr>
-//                     ))}
-//                 </tbody>
-//             </table>
-//         </div>
-//     </div>
-// }
-
-// export default F1DriverDetails;
+export default F1AllTeamsDetails;
