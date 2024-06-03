142e53e3fec811d3531feb34f64ec4539d66c27d
csiPIab <arianna.basaric@gmail.com>
2024-06-03 15:28:54 +0200

    header with errors - not finished,

--- src/Components/F1Breadcrumbs.js
+++ src/Components/F1Breadcrumbs.js
@@ -5,13 +5,10 @@ const F1Breadcrumbs = ({ items }) => {
     return <div>
         <ul> {items?.map((crumb, i) => {
             return (
-                <li className="crumb-btn"  key={i}>
-
-
-
-
+                <li key={i}>
                     {i === 0 && <img src={require("../img/icons/home.png")} style={{ maxWidth: 15 }} />}
                     {(i > 0) && (i < items.length - 1 )? (<Link to={crumb.path}>{crumb.name}</Link>) : (<span className="location-nav-btn"> {crumb.name} </span>)}
+                    {/* {(i < items.length - 1 )? (<Link to={crumb.path}>{crumb.name}</Link>) : (<span className="location-nav-btn"> {crumb.name} </span>)} */}
                 </li>
             );
         })}
