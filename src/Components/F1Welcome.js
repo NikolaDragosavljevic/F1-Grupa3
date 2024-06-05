45dd0ea15707b13b078bf5bae4a4a576ede8d37a
Ljubica Aleksic <ljubica_aleksic@yahoo.com>
2024-06-05 12:32:10 +0200

    minor wellcome update

--- src/Components/F1Welcome.js
+++ src/Components/F1Welcome.js
@@ -6,7 +6,9 @@ const F1Welcome = () => {
 
     return <div className="component-body welcome">
         <h1>Welcome : F1 FEEDER</h1>
-        <p></p>
+       <h2> <p>The drivers button shows which races driver drove and how many races, by cliking on the driver name we have more information and detail about the drivers biographies. By cliking on the race we have more information and details</p>
+        <p>The teams button shows constructons championship and details about racing</p>
+        <p>The races button shows race calendar 2013, details about round, grand prix, circuit, date and winner</p></h2>
         
 
         
