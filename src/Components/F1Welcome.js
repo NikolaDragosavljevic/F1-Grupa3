6e4ab67b05668b11888d97bc849fbe5615ba1352
jelena-droid <flowering.peaches@gmail.com>
2024-06-06 14:04:42 +0200

    welcome page is in progress

--- src/Components/F1Welcome.js
+++ src/Components/F1Welcome.js
@@ -6,23 +6,37 @@ const F1Welcome = (props) => {
 
     return <div className="component-body welcome">
         <h1>Welcome : F1 FEEDER</h1>
-        <p>The drivers button shows which races driver drove and how many races, by cliking on the driver name we have more information and detail about the drivers biographies. By cliking on the race we have more information and details</p>
-        <p>The teams button shows constructons championship and details about racing</p>
-        <p>The races button shows race calendar for selected year, details about round, grand prix, circuit, date and winner</p>
 
-        <select size="1" defaultValue={props.year} onChange={props.handler}>
-            {years.map(year => (
-                <option key={year}
-                    value={year}>
-                    {year}
-                </option>
-            ))}
-        </select>
 
-        <input type="button" value="Clear Local Storage" onClick={() => localStorage.clear()}/>;
+        <p>The <span>"Drivers"</span> button displays the races each driver participated in and the number of races they drove. By clicking on a driver's name, you can access more detailed information about their biography. Similarly, clicking on a specific race provides additional insights and details.</p>
+        <p>The <span>"Teams"</span> button showcases constructor championships and offers detailed information about racing teams.</p>
+        <p>The <span>"Races"</span>" button presents the race calendar for the selected year, including details about each round, Grand Prix, circuit, date, and the winner of each race.</p>
+
+
+        <div className="buttons">
+            <h2>Select The Year:</h2>
+            <select size="1" defaultValue={props.year} onChange={props.handler}>
+                {years.map(year => (
+                    <option key={year}
+                        value={year}>
+                        {year}
+                    </option>
+                ))}
+            </select>
+        </div>
+
+
+        <div className="buttons">
+            <h2>Clear Local Storage:</h2>
+            <input type="button" value="Clear Local Storage" onClick={() => localStorage.clear()} />;
+        </div>
 
         <img src={require(`../img/welcome.png`)} alt="F1Feeder" />
+
     </div>
+
 }
 
 export default F1Welcome;
+
+
